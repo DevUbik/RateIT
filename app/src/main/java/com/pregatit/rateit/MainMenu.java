9abdd100d43c1bc5c36d295d6a33f5ae2bda74e1
Vali <valistoicabusteni@gmail.com>
2022-04-10 19:14:54 +0300

    Am efectuat urmatoarele operatiuni: -Adaugat functionalitate pentru butoanele back(to create account), skip(to main menu); -Adaugat butone pe main menu : ajutor selectie( to ajutor selectie), log out ( to login screen); -Adaugat string-uri pentru numele butoanelor -Aranjat text.

--- app/src/main/java/com/pregatit/rateit/MainMenu.java
+++ app/src/main/java/com/pregatit/rateit/MainMenu.java
@@ -2,7 +2,9 @@ package com.pregatit.rateit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public void OnAjutorClick(View v)
+    {
+        Intent i = new Intent(this, ajutorSelectie.class);
+        startActivity(i);
+    }
+    public void OnLogOutClick(View v)
+    {
+        Intent i = new Intent(this, LoginPage.class);
+        startActivity(i);
     }
 }
