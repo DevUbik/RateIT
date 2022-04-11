31690a227f80ac1bf7f91cff97d935ded50401c6
DESKTOP-24Q5LH8\catalin <craciun.catalin92x@gmail.com>
2022-04-11 22:20:15 +0300

    'Insert product page' with back button which send you to mainMenu

--- app/src/main/java/com/pregatit/rateit/MainMenu.java
+++ app/src/main/java/com/pregatit/rateit/MainMenu.java
@@ -2,7 +2,9 @@ package com.pregatit.rateit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public void onInsertProductClick(View v) {
+        Intent i = new Intent(this, InsertProductPage.class);
+        startActivity(i);
     }
 }
