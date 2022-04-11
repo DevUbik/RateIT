ad8eb8e78fca2816cde29cc745cda9d1ca7b87ca
iridiffinis <102820231+iridiffinis@users.noreply.github.com>
2022-04-11 21:14:10 +0300

    Added a "Settings" button in activity_main_menu.xml and made it redirect to the activity_settings_page.xml. Also made the "back" button in activity_settings_page.xml redirect to the activity_main_menu.xml. Changed the text attributes in activity_settings_page.xml to the respective string.

--- app/src/main/java/com/pregatit/rateit/SettingsPage.java
+++ app/src/main/java/com/pregatit/rateit/SettingsPage.java
@@ -2,7 +2,10 @@ package com.pregatit.rateit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class SettingsPage extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class SettingsPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_page);
+    }
+
+    public void OnImageButtonClick(View v)
+    {
+        Intent i = new Intent(this, MainMenu.class);
+        startActivity(i);
     }
 }
