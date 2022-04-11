057c4904bdb1208631c2c66905535c22e6dcfa3c
DESKTOP-NREO4PI\Claudia <ciutacu51@gmail.com>
2022-04-11 16:02:33 +0300

    Product Catalog

--- app/src/main/java/com/pregatit/rateit/ProductCatalog.java
+++ app/src/main/java/com/pregatit/rateit/ProductCatalog.java
@@ -2,7 +2,9 @@ package com.pregatit.rateit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ProductCatalog extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ProductCatalog extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_catalog2);
+    }
+
+    public void onBackClick(View v){
+        Intent i = new Intent(this, MainMenu.class);
+        startActivity(i);
     }
 }
