25831097d3cdd902b7c02e7550684641acb6c5da
iridiffinis <102820231+iridiffinis@users.noreply.github.com>
2022-04-19 13:22:21 +0300

    Added a listener to the sound switch to remember if is checked or unchecked.

--- app/src/main/java/com/pregatit/rateit/SettingsPageActivity.java
+++ app/src/main/java/com/pregatit/rateit/SettingsPageActivity.java
@@ -2,9 +2,12 @@ package com.pregatit.rateit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,6 +17,28 @@ public class SettingsPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_page);
+
+        Switch soundSwitch = findViewById(R.id.soundSwitch);
+
+        SharedPreferences shared =this.getSharedPreferences("RateIt",Context.MODE_PRIVATE);
+
+        soundSwitch.setChecked(shared.getBoolean("value",true));
+        soundSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = shared.edit();
+                if(soundSwitch.isChecked())
+                {
+                    editor.putBoolean("value",true);
+                    editor.apply();
+                    soundSwitch.setChecked(true);
+                }else {
+                    editor.putBoolean("value",false);
+                    editor.apply();
+                    soundSwitch.setChecked(false);
+                }
+            }
+        });
 
         TextView editProfile = (TextView) findViewById(R.id.edit_profileText);
         editProfile.setOnClickListener(new View.OnClickListener() {
@@ -37,4 +62,6 @@ public class SettingsPageActivity extends AppCompatActivity {
         Intent i = new Intent(this, MainMenuActivity.class);
         startActivity(i);
     }
+
+
 }
